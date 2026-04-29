6a61d9f9292bf0f60133f35f68aefe9382726a70
birklazar <birk_lazar@hotmail.dk>
2026-04-29 09:06:27 +0200

    backend til chat_funktion

--- backend/src/controllers/messageController.js
+++ backend/src/controllers/messageController.js
@@ -1,34 +1,55 @@
 import { Message } from "../models/Message.model.js";
 
-const sendMessage = async (req, res) =>{
-    try {
-        const {sender, reciver, text} = req.body;
-        if (!sender || !reciver || !text){
-            return res.status(400).json({message: "All fields are required"});
-        }
-        const message = await Message.create({sender, reciver, text});
-        res.status(201).json(message);
+const sendMessage = async (req, res) => {
+  try {
+    const { sender, receiver, post, text } = req.body;
 
-    } catch (error) {
-        res.status(500).json({message: "Server error"});
+    if (!sender || !receiver || !post || !text) {
+      return res.status(400).json({ message: "All fields are required" });
     }
+    if (sender === receiver) {
+  return res.status(400).json({ message: "You cannot message yourself" });
 }
 
-const getMessage = async (req, res) =>{
-    try {
-        const loggedInUserId = req.query.senderId;
-        const otherUserId = req.params.userId;
+    const message = await Message.create({
+      sender,
+      receiver,
+      post,
+      text,
+    });
 
-        const messages = await Message.find({
-            $or: [
-                {sender: loggedInUserId, receiver: otherUserId},
-                {sender: otherUserId, receiver: loggedInUserId}
-            ],
+    res.status(201).json(message);
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+      error: error.message,
+    });
+  }
+};
 
-        }).sort({createdAt: 1});
-        res.status(200).json(messages);
-    } catch (error) {
-        res.status(500).json({message: "Server error"});
-    }
-}
-export {sendMessage, getMessage};
+const getMessage = async (req, res) => {
+  try {
+    const loggedInUserId = req.query.senderId;
+    const otherUserId = req.params.userId;
+
+    const messages = await Message.find({
+      $or: [
+        { sender: loggedInUserId, receiver: otherUserId },
+        { sender: otherUserId, receiver: loggedInUserId },
+      ],
+    })
+      .populate("sender", "username email")
+      .populate("receiver", "username email")
+      .populate("post", "title")
+      .sort({ createdAt: 1 });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+      error: error.message,
+    });
+  }
+};
+
+export { sendMessage, getMessage };
